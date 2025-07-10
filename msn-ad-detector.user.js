3b389dfca8ce3910fe38d0786a16b3dc793b06d6
sungcheol <sungcheol@dable.io>
2025-07-10 19:37:38 +0900

    update version

--- msn-ad-detector.user.js
+++ msn-ad-detector.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         MSN에서 광고 찾기
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Detect and highlight ad providers on MSN Korea
 // @author       sungcheol-dable
 // @match        https://www.msn.com/ko-kr
 // @match        https://www.msn.com/ko-kr/
-// @updateURL    https://cdn.jsdelivr.net/gh/sungcheol-dable/msn-helper@20250710-0731/msn-ad-detector.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/sungcheol-dable/msn-helper@20250710-0731/msn-ad-detector.user.js
-// @require      https://cdn.jsdelivr.net/gh/sungcheol-dable/msn-helper@20250710-0731/main.js
+// @updateURL    https://cdn.jsdelivr.net/gh/sungcheol-dable/msn-helper@202507100737/msn-ad-detector.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/sungcheol-dable/msn-helper@202507100737/msn-ad-detector.user.js
+// @require      https://cdn.jsdelivr.net/gh/sungcheol-dable/msn-helper@202507100737/main.js
 // @grant        none
 // ==/UserScript==
